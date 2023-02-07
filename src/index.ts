00293c92febb55bcaf8569797a22bfcdf5dc543e
YusukeMizutani11 <yusuke.mizutani@smail.astate.edu>
2023-02-06 18:17:51 -0600

    making StudentController done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express, { Express } from 'express';
-import { notImplemented } from './controllers/NotImplementedController';
+import {
+  getAllStudents,
+  createNewStudent,
+  getStudentByName,
+} from './controllers/StudentController';
 
 const app: Express = express();
 const PORT = 8091;
 
-app.post('/api/students', notImplemented);
-app.get('/api/students/:studentName', notImplemented);
+app.use(express.json());
+
+app.get('/', getAllStudents);
+
+app.post('/students', createNewStudent);
+app.get('/students/:studentName', getStudentByName);
 
 app.listen(PORT, () => {
   console.log(`Server listening on https://localhost:${PORT}`);
